064a1fa0eb2f873c56f2f5132a857959c7c54c88
Dyaln Reyes <dylanreyessegura@gmail.com>
2024-07-29 18:46:16 +0200

    cambios

--- mygame.cpp
+++ mygame.cpp
@@ -1,3 +1,5 @@
+//prueba
+
 #include<iostream>
 #include<conio.h>
 #include<dos.h> 
@@ -16,10 +18,10 @@ COORD CursorPosition;
 int enemyY[3];
 int enemyX[3];
 int enemyFlag[3];
-char car[4][4] = { ' ','±','±',' ', 
-					'±','±','±','±', 
-					' ','±','±',' ',
-					'±','±','±','±' }; 
+char car[4][4] = { ' ','ï¿½','ï¿½',' ', 
+					'ï¿½','ï¿½','ï¿½','ï¿½', 
+					' ','ï¿½','ï¿½',' ',
+					'ï¿½','ï¿½','ï¿½','ï¿½' }; 
 					
 int carPos = WIN_WIDTH/2;
 int score = 0; 
@@ -41,12 +43,12 @@ void setcursor(bool visible, DWORD size) {
 void drawBorder(){  
 	for(int i=0; i<SCREEN_HEIGHT; i++){
 		for(int j=0; j<17; j++){
-			gotoxy(0+j,i); cout<<"±";
-			gotoxy(WIN_WIDTH-j,i); cout<<"±";
+			gotoxy(0+j,i); cout<<"ï¿½";
+			gotoxy(WIN_WIDTH-j,i); cout<<"ï¿½";
 		}
 	} 
 	for(int i=0; i<SCREEN_HEIGHT; i++){
-		gotoxy(SCREEN_WIDTH,i); cout<<"±";
+		gotoxy(SCREEN_WIDTH,i); cout<<"ï¿½";
 	} 
 }
 void genEnemy(int ind){
